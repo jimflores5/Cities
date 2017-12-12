49aea5a992501693e9c24be0c67417de3812611d
Jim Flores <jimflores5@gmail.com>
2017-12-11 20:19:02 -0600

    Completed C# class 11 "Cities" studio.

---
+++ src/Cities/Comparers/AreaComparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cities.Comparers
+{
+    class AreaComparer : IComparer<City>
+    {
+        public int Compare(City x, City y)
+        {
+            if (x.Area < y.Area)
+            {
+                return 1;
+            }
+            else if (x.Area > y.Area)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
--- src/Cities/Program.cs
+++ src/Cities/Program.cs
@@ -9,9 +9,37 @@ namespace Cities
         public static void Main(string[] args)
         {
             List<City> cities = CityDataImporter.LoadData();
+            IComparer<City> comparer;
+
+            Console.WriteLine("Choose to sort by: \n" +
+                "1) City name \n" +
+                "2) State name \n" +
+                "3) Population \n" +
+                "4) Area");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                comparer = new NameComparer();
+            }
+            else if(choice == "2")
+            {
+                comparer = new StateComparer();
+            }
+            else if (choice == "3")
+            {
+                comparer = new PopulationComparer();
+            }
+            else
+            {
+                comparer = new AreaComparer();
+            }
 
             // TODO Swap out comparers as desired
-            IComparer<City> comparer = new NameComparer();
+            //IComparer<City> comparer = new NameComparer();
+            //IComparer<City> comparer = new StateComparer();
+            //IComparer<City> comparer = new PopulationComparer();
+            //IComparer<City> comparer = new AreaComparer();
 
             cities.Sort(comparer);
 
@@ -28,7 +56,6 @@ namespace Cities
             {
                 Console.WriteLine(city);
             }
-
         }
     }
 }
